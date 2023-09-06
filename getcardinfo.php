e661be2671d1cb9374084934d0983ba36400ed1d
Danny Wall <danny@dewdevelopment.com>
2023-09-06 09:48:26 -0700

    modified:   card_insert.php 	modified:   getcardinfo.php 	modified:   step3.php

--- getcardinfo.php
+++ getcardinfo.php
@@ -1,19 +1,38 @@
-<?php
-$host = "localhost";
-$dbuser = "dew_thoth";
-$dbpass = "1!Iktomi!1";
-$dbname = "dew_thoth";
-$link = mysqli_connect($host, $dbuser, $dbpass, $dbname);
-$card = $_GET['c'];
-$the_orientation = $_GET['o'];
-if ($the_orientation == "upright"){
-    $orientation = 1;
-} else {
-    $orientation = 2;
-}
-$sql = "select * from deck where name like '$card' and orientation = $orientation";
-$result = mysqli_query($link, $sql);
-$row = mysqli_fetch_assoc($result);
-$meaning = $row['meaning'];
-echo ("<h1>$card - $the_orientation</h1>$meaning");
-?>
+<?php
+
+$host = "localhost";
+
+$dbuser = "db_username";
+
+$dbpass = "db_password";
+
+$dbname = "db_name";
+
+$link = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+$card = $_GET['c'];
+
+$the_orientation = $_GET['o'];
+
+if ($the_orientation == "upright"){
+
+    $orientation = 1;
+
+} else {
+
+    $orientation = 2;
+
+}
+
+$sql = "select * from deck where name like '$card' and orientation = $orientation";
+
+$result = mysqli_query($link, $sql);
+
+$row = mysqli_fetch_assoc($result);
+
+$meaning = $row['meaning'];
+
+echo ("<h1>$card - $the_orientation</h1>$meaning");
+
+?>
+
